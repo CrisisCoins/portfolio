59d4e4db1eef5d2fc1630f38e8e20299dafd370b
0x4a5 <github@crisiscoins.com>
2022-06-08 21:42:00 -0700

    added about component

---
+++ components/About.jsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+
+const About = () => {
+  return (
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#41495c]">
+            About
+          </p>
+          <h2 className="py-4 ">Who Am I</h2>
+          <p className="py-2 text-[#878883]">
+            I hold dual M.S. Degrees in Information Technology and have spent
+            the last 20 years in the IT Field working as a professional
+            enterprise engineer and working in other various IT positions. I
+            have always had a passion for development, especially the creativity
+            aspect and its final result. In 2019 Covid changed the world, and I
+            utilized this time to submerse myself into fine tunning my HTML &
+            CSS skills in order to use them within my startup business. In
+            actuality the fine tuning has now become a new found passion and
+            love for Fullstack development.
+          </p>
+          <p className="py-2 text-[#878883]">
+            Captivated by the unbounded creativity programming allow you to
+            implement, I was instantly attracted and wanted to learn more. I
+            started taking courses and building on previously acquired knowledge
+            by learning javascript and was even more fascinated with developing
+            websites interactive. I currently started to do freelancing with a
+            focus on Fullstack, Blockchain and mobile development applications.
+            I am now spending my time building projects with Next JS, React JS,
+            Firebase, and am constantly learning new technologies.
+          </p>
+          <p className="py-2 text-[#878883] underline cursor-pointer">
+            Here are my projects...
+          </p>
+        </div>
+        <div className="w-full h-auto m-auto bg-white text-center rounded-3xl border border-gray-300 shadow-lg p-10 max-w-xs hover:scale-105 ease-in duration-300">
+          <Image
+            className="mb-3 w-32 h-32 rounded-full border mx-auto"
+            src="/../public/assets/designer.png"
+            alt="/"
+            width="100"
+            height="100"
+          />
+          <h1 className="text-lg text-[#34393f]"> Michael Levi </h1>
+          <h3 className="text-sm text-[#878883]"> FullStack Developer </h3>
+          <p className="text-xs text-[#878883] mt-4">
+            Well-qualified Certified Full Stack Developer familiar with wide
+            range of programming utilities and languages. Knowledgeable of
+            backend and frontend development requirements. Handles any part of
+            process with ease. Collaborative team player with excellent
+            technical abilities offering 19 years of related experience.
+          </p>
+          <Link href="mailto:development@crisiscoins.com">
+            <button
+              className="border border-[#41495c] shadow-md shadow-[#34393f]/40 
+                        text-[#878883] mt-8 hover:scale-110 ease-in transition duration-300 cursor-pointer"
+            >
+              Hire Me
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from 'next/link'
+import Link from "next/link";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsCalendarWeek } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
@@ -8,61 +8,61 @@ import Typed from "react-typed";
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
-    <div className="max-w-[] w-full h-full mx-auto p-2 flex justify-center items-center">
-      <div>
-        <p className="uppercase text-sm tracking-widest text-[#878883]">
-          Building tomorrow, today...
-        </p>
-        <h1 className="py-4 text-[#878883]">
-          Hi, my name is
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#878883] to-[#41495c]">
-            Michael
-          </span>
-        </h1>
-        <h1 className="py-2 text-[#878883]">And I am a FullStack</h1>
+      <div className="max-w-[] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <Typed
-            className="text-4xl sm:text-7xl text-[#41495c]"
-            strings={["Web", "Blockchain", "Mobile", "Developer."]}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
-        </div>
-        <p className="py-4 text-[#878883] max-w-[70%] m-auto">
-          I am a full-stack developer specializing in web, mobile and
-          blockchain software solutions. I focused on providing exception
-          customer service and building long term business relations. I offer
-          both personal and multi scale business software solutions. I look
-          forward to helping you build your next project.
-        </p>
-        {/* Selection Media Block */}
-        <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-          <Link href="https://www.linkedin.com/in/crisiscoins">
-            <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaLinkedinIn />
-            </div>
-          </Link>
-          <Link href="https://github.com/">
-            <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaGithub />
-            </div>
-          </Link>
-          <Link href="mailto:development@crisiscoins.com">
-            <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <AiOutlineMail />
-            </div>
-          </Link>
-          <Link href="https://calendly.com/michael-levi?hide_landing_page_details=1&hide_gdpr_banner=1">
-            <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <BsCalendarWeek />
-            </div>
-          </Link>
+          <p className="uppercase text-sm tracking-widest text-[#878883]">
+            Building tomorrow, today...
+          </p>
+          <h1 className="py-4 text-[#878883]">
+            Hi, my name is
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#878883] to-[#41495c]">
+              Michael
+            </span>
+          </h1>
+          <h1 className="py-2 text-[#878883]">And I am a FullStack</h1>
+          <div>
+            <Typed
+              className="text-4xl sm:text-7xl text-[#41495c]"
+              strings={["Web", "Blockchain", "Mobile", "Developer."]}
+              typeSpeed={120}
+              backSpeed={140}
+              loop
+            />
+          </div>
+          <p className="py-4 text-[#878883] max-w-[70%] m-auto">
+            I am a full-stack developer specializing in web, mobile and
+            blockchain software solutions. I focused on providing exception
+            customer service and building long term business relations. I offer
+            both personal and multi scale business software solutions. I look
+            forward to helping you build your next project.
+          </p>
+          {/* Selection Media Block */}
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <Link href="https://www.linkedin.com/in/crisiscoins">
+              <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaLinkedinIn />
+              </div>
+            </Link>
+            <Link href="https://github.com/">
+              <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaGithub />
+              </div>
+            </Link>
+            <Link href="mailto:development@crisiscoins.com">
+              <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <AiOutlineMail />
+              </div>
+            </Link>
+            <Link href="https://calendly.com/michael-levi?hide_landing_page_details=1&hide_gdpr_banner=1">
+              <div className="rounded-full shadow-md shadow-[#34393f]/40 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <BsCalendarWeek />
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
