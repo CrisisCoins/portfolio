0448b777d20e3020d6bd04828104d7da0e073bf0
0x4a5 <github@crisiscoins.com>
2022-06-09 20:30:43 -0700

    modified links to projects

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -38,7 +38,7 @@ const portfolio = () => {
               <button className="px-8 py-2 mt-4 mr-8">Demo</button>
             </a>
           </Link>
-          <Link href="https://github.com/0x4a5/portfolio-v1">
+          <Link href="https://github.com/0x4a5/portfolio">
             <a>
               <button className="px-8 py-2 mt-4">Code</button>
             </a>
